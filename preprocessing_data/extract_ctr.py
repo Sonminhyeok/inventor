1ff8727a3c20fd947778ef89f28b51050b31def3
Sonminhyeok <cliff615@naver.com>
2024-03-19 17:05:33 +0900

    0319.1705

--- preprocessing_data/extract_ctr.py
+++ preprocessing_data/extract_ctr.py
@@ -2,21 +2,14 @@ import re
 import pandas as pd
 def extract_kr(df):
     korean_pattern = re.compile('[\u3131-\u3163\uac00-\ud7a3]+')
-    kr_df = df[df["출원인국적"] == "KR"]
+    kr_df = df[df["출원인국적"].str.contains("KR")]
     kr_korean_df = kr_df[kr_df["발명자"].apply(lambda x: korean_pattern.search(x) is not None)]
     return kr_korean_df
-def extract_samename_list(df):
-    name_list = []
-    for name in df["발명자"]:
-        if isinstance(name, str) and "|" in name:
-            name_list.extend(name.split("|"))
-        else:
-            name_list.append(name)
-    duplicates = set([x for x in name_list if name_list.count(x) > 1])
-    if duplicates:
-        print(f"중복된 이름: {', '.join(duplicates)}")
-    else:
-        print("중복된 이름이 없습니다.")
+def extract_samename(df, column):
+    exploded_df = df.assign(발명자=df[column].str.split("|")).explode(column, ignore_index=True)
+    duplicate_names = exploded_df[exploded_df.duplicated(subset=[column], keep=False)]
+    duplicate_names=duplicate_names.sort_values(by=column)
+    return duplicate_names
 def merge_to_csv(df1,df2,overlap_col, target_col):
     common_skeys = df2[df2[overlap_col].isin(df1[overlap_col])]
     merged_df = pd.merge(df1, df2, on=overlap_col, how='left')
@@ -25,4 +18,8 @@ def merge_to_csv(df1,df2,overlap_col, target_col):
     merged_df[target_col] = merged_df[target_col].fillna(merged_df[target_col])
     sorted_df = merged_df.sort_values(by='출원일')
     return sorted_df
-    
+def explode_split(df,column):
+    df[column] = df[column].str.split('|')
+    split_df=df.explode(column)
+    return split_df
+
--- preprocessing_data/utils.py
+++ preprocessing_data/utils.py
@@ -15,3 +15,18 @@ def str_to_datetime(str):
     return datetime.datetime.strptime(str, "%Y-%m-%d")
 def datetime_to_str(date):
     return datetime.datetime.strftime(date,"%Y-%m-%d")
+
+def find_common_address(address, previous_address):
+    address_list=address.split("|")
+    previous_address_list=previous_address.split("|")
+    common_address_list=[]
+    for address1 in address_list:
+        for address2 in previous_address_list:
+            if address1 == address2:
+                common_address_list.append(address1)
+    if common_address_list:
+        common_address= "|".join(common_address_list)
+        return common_address
+    else:
+        return "No"
+    return common_address
